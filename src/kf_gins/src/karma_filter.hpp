ff71f3418debd998040095e546bee49c4caf0be4
aurora <1667157382@qq.com>
2025-05-06 13:43:46 +0800

    3

--- src/kf_gins/src/karma_filter.hpp
+++ src/kf_gins/src/karma_filter.hpp
@@ -6,13 +6,14 @@
 
 /*
  * 把这个头文件和源代码放一个目录下不合理，这里只是为了演示方便
+ * 
 */
 
 // to doooooooooooooo
 class Filter{
 public:
-  // 构造函数
-  Filter();
+  // 构造函数，explicit表示只能显示的调用构造函数创建一个滤波器对象
+  explicit Filter();
   
   // 更新IMU数据，完成一次惯导推算
   void updateImu();
