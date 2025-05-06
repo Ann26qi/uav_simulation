d7fb3bcc35cdea4800b729cd57416ecfc5027abe
aurora <1667157382@qq.com>
2025-05-06 12:56:08 +0800

    changes

--- src/kf_gins/src/kf_gins_node.cpp
+++ src/kf_gins/src/kf_gins_node.cpp
@@ -5,14 +5,21 @@
 #include "karma_filter.hpp"
 
 
+/*              还需要完成的地方
+ * 在完成计算后，应该创建一个发布者将计算的导航量发布
+*/
+
 class KfGnssIns : public rclcpp::Node
 {
 public:
   explicit KfGnssIns() : Node("kf_gnss_ins")
   {
+
+    // 配置通信质量
     rmw_qos_profile_t qos_profile = rmw_qos_profile_sensor_data;
     auto qos = rclcpp::QoS(rclcpp::QoSInitialization(qos_profile.history, 5), qos_profile);
     
+    // 创建订阅对象
     imu_sub_ = this->create_subscription<px4_msgs::msg::SensorCombined>(
       "/fmu/out/sensor_combined", qos, 
       std::bind(&KfGnssIns::imuCallback, this, std::placeholders::_1));
@@ -28,7 +35,8 @@ private:
 
   void imuCallback(const px4_msgs::msg::SensorCombined::SharedPtr msg);
   void gnssCallback(const px4_msgs::msg::SensorGnssRelative::SharedPtr msg);
-
+  
+  // 滤波器对象
   Filter filter_;
   
 };
@@ -36,13 +44,19 @@ private:
 
 void KfGnssIns::imuCallback(const px4_msgs::msg::SensorCombined::SharedPtr msg){
   // to-do
+
+  // 更新IMU数据
   filter_.updateImu();
   // to-do
 }
 void kfGnssIns::gnssCallback(const px4_msgs::msg::SensorGnssRelative::SharedPtr msg){
   // to-do
+
+  // 更新GNSS数据
   filter_.updateGnss();
+  // 完成一次互补滤波
   filter_.fused_gnss_ins();
+
   // to-do
 }
 
