9af896785dec686b30da3ea1321a61303f435729
aurora <1667157382@qq.com>
2025-05-06 13:29:19 +0800

    2

--- src/kf_gins/src/karma_filter.hpp
+++ src/kf_gins/src/karma_filter.hpp
@@ -3,9 +3,26 @@
 #include<Eigen/Dense>
 
 
+
+/*
+ * 把这个头文件和源代码放一个目录下不合理，这里只是为了演示方便
+*/
+
 // to doooooooooooooo
 class Filter{
 public:
-  void init(){}
+  // 构造函数
+  Filter();
   
+  // 更新IMU数据，完成一次惯导推算
+  void updateImu();
+
+  // 更新GNSS数据
+  void updateGnss();
+
+  // 判断是否需要对IMU数据内插
+  bool is_toInterpolate();
+
+  // 完成一次互补滤波算法，并进行误差反馈
+  void fused_gnss_ins();
 }
